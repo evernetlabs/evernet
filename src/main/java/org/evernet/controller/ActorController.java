7444155045173b76e051f59686a941ec376e1552
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2025-08-05 22:58:27 +0530

    added actor account management apis

--- src/main/java/org/evernet/controller/ActorController.java
+++ src/main/java/org/evernet/controller/ActorController.java
@@ -1,17 +1,18 @@
 package org.evernet.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.evernet.auth.AuthenticatedActorController;
 import org.evernet.model.Actor;
+import org.evernet.request.ActorPasswordChangeRequest;
+import org.evernet.request.ActorUpdateRequest;
 import org.evernet.service.ActorService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
-public class ActorController  extends AuthenticatedActorController {
+public class ActorController extends AuthenticatedActorController {
 
     private final ActorService actorService;
 
@@ -19,5 +20,23 @@ public class ActorController  extends AuthenticatedActorController {
     public Actor get() {
         checkLocal();
         return actorService.get(getActorNodeIdentifier(), getActorIdentifier());
+    }
+
+    @PutMapping("/actors/current")
+    public Actor update(@Valid @RequestBody ActorUpdateRequest request) {
+        checkLocal();
+        return actorService.update(getActorNodeIdentifier(), getActorIdentifier(), request);
+    }
+
+    @PutMapping("/actors/current/password")
+    public Actor changePassword(@Valid @RequestBody ActorPasswordChangeRequest request) {
+        checkLocal();
+        return actorService.changePassword(getActorNodeIdentifier(), getActorIdentifier(), request);
+    }
+
+    @DeleteMapping("/actors/current")
+    public Actor delete() {
+        checkLocal();
+        return actorService.delete(getActorNodeIdentifier(), getActorIdentifier());
     }
 }
--- src/main/java/org/evernet/service/ActorService.java
+++ src/main/java/org/evernet/service/ActorService.java
@@ -12,8 +12,10 @@ import org.evernet.exception.NotFoundException;
 import org.evernet.model.Actor;
 import org.evernet.model.Node;
 import org.evernet.repository.ActorRepository;
+import org.evernet.request.ActorPasswordChangeRequest;
 import org.evernet.request.ActorSignUpRequest;
 import org.evernet.request.ActorTokenRequest;
+import org.evernet.request.ActorUpdateRequest;
 import org.evernet.response.ActorTokenResponse;
 import org.evernet.util.Ed25519KeyHelper;
 import org.evernet.util.Password;
@@ -93,6 +95,34 @@ public class ActorService {
             throw new NotFoundException(String.format("Actor %s not found on node %s", identifier, nodeIdentifier));
         }
 
+        return actor;
+    }
+
+    public Actor update(String nodeIdentifier, String identifier, ActorUpdateRequest request) {
+        Actor actor = get(nodeIdentifier, identifier);
+
+        actor.setDescription(request.getDescription());
+
+        if (StringUtils.hasText(request.getDisplayName())) {
+            actor.setDisplayName(request.getDisplayName());
+        }
+
+        if (StringUtils.hasText(request.getType())) {
+            actor.setType(request.getType());
+        }
+
+        return actorRepository.save(actor);
+    }
+
+    public Actor changePassword(String nodeIdentifier, String identifier, ActorPasswordChangeRequest request) {
+        Actor actor = get(nodeIdentifier, identifier);
+        actor.setPassword(Password.hash(request.getPassword()));
+        return actorRepository.save(actor);
+    }
+
+    public Actor delete(String nodeIdentifier, String identifier) {
+        Actor actor = get(nodeIdentifier, identifier);
+        actorRepository.delete(actor);
         return actor;
     }
 }
