db54afa65a6c759f9e1c27c9b1a7ed8220e76c94
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2025-10-04 23:04:18 +0530

    added health check

---
+++ vertex/health/health_api.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+class HealthApi:
+    def __init__(self, app: Flask):
+        self.app = app
+
+    def register(self):
+
+        @self.app.get("/health")
+        def health_check_api():
+            return {
+                "status": "ok",
+            }
---
+++ vertex_server.py
@@ -0,0 +1,49 @@
+import os
+import plyvel
+
+from dotenv import *
+from flask import Flask, request, jsonify, g
+
+from vertex.health.health_api import HealthApi
+
+load_dotenv()
+data_path = os.getenv("DATA_PATH")
+
+print("initializing data path...")
+if not os.path.exists(data_path):
+    os.makedirs(data_path)
+
+print("initializing database...")
+if not os.environ.get("WERKZEUG_RUN_MAIN"):
+    print("Skipping DB init in reloader process")
+else:
+    db = plyvel.DB(f'{data_path}/evernet.db', create_if_missing=True)
+
+app = Flask(__name__)
+
+HealthApi(app).register()
+
+
+@app.before_request
+def before_request():
+    g.request_body = request.get_json(force=True, silent=True)
+
+
+@app.errorhandler(404)
+def handle_404_error(e):
+    return jsonify({
+        "success": False,
+        "message": str(e)
+    }), 404
+
+
+@app.errorhandler(Exception)
+def handle_all_errors(e):
+    return jsonify({
+        "success": False,
+        "message": str(e)
+    }), 500
+
+
+if __name__ == '__main__':
+    app.run(host=os.getenv("HOST"), port=int(os.getenv("PORT")), debug=os.getenv("ENV") != "PROD")
