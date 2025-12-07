d8ef62f0cb6e5cab2df750302722b111dc27e91d
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2025-12-07 22:04:19 +0530

    added node management apis

--- controller/node_controller.py
+++ controller/node_controller.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from service.node_service import NodeService
-from utils.api_utils import required_param, authenticate_admin
+from utils.api_utils import optional_param, pagination_page, pagination_size, required_param, authenticate_admin
 
 class NodeController:
     def __init__(self, app: Flask, node_service: NodeService) -> None:
@@ -13,8 +13,44 @@ class NodeController:
         @authenticate_admin()
         def create_node(admin):
             return self.node_service.create(
-                required_param("identifier"),
-                required_param("display_name"),
-                required_param("description"),
+                required_param("identifier", str),
+                required_param("display_name", str),
+                required_param("description", str),
+                required_param("open", bool),
                 admin["identifier"]
             )
+
+        @self.app.get("/api/v1/admins/nodes")
+        @authenticate_admin()
+        def fetch_nodes(_):
+            return self.node_service.fetch(
+                pagination_page(),
+                pagination_size()
+            )
+
+        @self.app.get("/api/v1/admins/nodes/<identifier>")
+        @authenticate_admin()
+        def get_node(_, identifier: str):
+            return self.node_service.get(identifier)
+
+        @self.app.put("/api/v1/admins/nodes/<identifier>")
+        @authenticate_admin()
+        def update_node(_, identifier: str):
+            return self.node_service.update(
+                identifier,
+                optional_param("display_name", str),
+                optional_param("description", str)
+            )
+
+        @self.app.put("/api/v1/admins/nodes/<identifier>/open")
+        @authenticate_admin()
+        def update_node_open(_, identifier: str):
+            return self.node_service.update_open(
+                identifier,
+                required_param("open", bool)
+            )
+
+        @self.app.delete("/api/v1/admins/nodes/<identifier>")
+        @authenticate_admin()
+        def delete_node(_, identifier: str):
+            return self.node_service.delete(identifier)
--- service/node_service.py
+++ service/node_service.py
@@ -7,7 +7,7 @@ class NodeService:
     def __init__(self, mongo: Collection) -> None:
         self.mongo = mongo
     
-    def create(self, identifier: str, display_name: str, description: str, creator: str) -> dict:
+    def create(self, identifier: str, display_name: str, description: str, open: bool, creator: str) -> dict:
         if self.mongo.count_documents({
             "identifier": identifier
         }) > 0:
@@ -19,6 +19,7 @@ class NodeService:
             "identifier": identifier,
             "display_name": display_name,
             "description": description,
+            "open": open,
             "private_key": private_key_to_string(private_key),
             "public_key": public_key_to_string(public_key),
             "creator": creator,
@@ -30,18 +31,81 @@ class NodeService:
             "identifier": identifier
         }
     
-    def fetch(self) -> list[dict]:
-        pass
+    def fetch(self, page: int = 0, size: int = 50) -> list[dict]:
+        nodes = self.mongo.find().skip(page * size).limit(size)
+        return [self.to_dict(node) for node in nodes]
     
     def get(self, identifier: str) -> dict:
-        pass
+        node = self.mongo.find_one({
+            "identifier": identifier
+        })
+
+        if not node:
+            raise Exception(f"Node {identifier} not found")
+        return self.to_dict(node)
         
     def update(self, identifier: str, display_name: str, description: str) -> dict:
-        pass
+        fields = {
+            "updated_at": current_datetime()
+        }
+
+        if display_name:
+            fields["display_name"] = display_name
+        if description:
+            fields["description"] = description
+
+        result = self.mongo.update_one({
+            "identifier": identifier
+        }, {
+            "$set": fields
+        })
+
+        if result.matched_count == 0:
+            raise Exception(f"Node {identifier} not found")
+
+        return {
+            "identifier": identifier
+        }
+
+    def update_open(self, identifier: str, open: bool) -> dict:
+        fields = {
+            "updated_at": current_datetime(),
+            "open": open
+        }
+
+        result = self.mongo.update_one({
+            "identifier": identifier
+        }, {
+            "$set": fields
+        })
+
+        if result.matched_count == 0:
+            raise Exception(f"Node {identifier} not found")
+
+        return {
+            "identifier": identifier
+        }
     
     def delete(self, identifier: str) -> dict:
-        pass
+        result = self.mongo.delete_one({
+            "identifier": identifier
+        })
+
+        if result.deleted_count == 0:
+            raise Exception(f"Node {identifier} not found")
+
+        return {
+            "identifier": identifier
+        }
     
     @staticmethod
-    def to_dict(self) -> dict:
-        pass
+    def to_dict(node: dict) -> dict:
+        return {
+            "identifier": node.get("identifier"),
+            "display_name": node.get("display_name"),
+            "description": node.get("description"),
+            "open": node.get("open"),
+            "creator": node.get("creator"),
+            "created_at": node.get("created_at"),
+            "updated_at": node.get("updated_at")
+        }
