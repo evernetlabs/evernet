e4377b90d7690d4623f30718b1f35c86bde037c7
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2026-03-13 13:34:21 +0530

    Added service template

---
+++ util/api.py
@@ -0,0 +1,33 @@
+from flask import g, request
+
+
+def required_param(key: str, data_type=str):
+    if not g.request_body:
+        raise Exception("Request body is missing")
+    if key not in g.request_body:
+        raise Exception(f"{key} is required")
+    val = g.request_body[key]
+    if not isinstance(val, data_type):
+        raise Exception(f"Invalid data type for value of {key}")
+    return val
+
+
+def optional_param(key: str, data_type=str):
+    if not g.request_body:
+        return None
+    if key not in g.request_body:
+        return None
+    val = g.request_body[key]
+    if not val:
+        return None
+    if not isinstance(val, data_type):
+        raise Exception(f"Invalid data type for value of {key}")
+    return val
+
+
+def pagination_page():
+    return request.args.get("page", 0, int)
+
+
+def pagination_size():
+    return request.args.get("size", 50, int)
---
+++ util/secret.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+
+def generate_secret(n):
+    characters = string.ascii_letters + string.digits
+    random_string = ''.join(random.choices(characters, k=n))
+    return random_string
